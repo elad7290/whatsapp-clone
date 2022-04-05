883f67aa210bf3dad22b6758722afc7bad5ca58d
Chen Larry <chen.larry@live.biu.ac.il>
2022-04-05 13:34:00 +0300

    divide and conquer!

--- src/Components/LoginForms/LoginForm.js
+++ src/Components/LoginForms/LoginForm.js
@@ -3,22 +3,30 @@ import TextInput from "../LoginInputs/TextInput";
 import PasswordInput from "../LoginInputs/PasswordInput";
 import SubmitButton from "../SubmitButton/SubmitButton";
 import {Link} from "react-router-dom";
-import Users from "../../Server/Users";
+import GetUser from "../../Server/Users";
+import Alert from "../Alert/Alert";
 
 function LoginForm() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const name = document.getElementById('username').value;
+        const username = document.getElementById('username').value;
         const pwd = document.getElementById('password').value;
-        if(Users.some((user) => { return (user.username === name && user.password===pwd) })){
+        /*if(Users.some((user) => { return (user.username === name && user.password===pwd) })){
             console.log("yayyy");
+        }*/
+        const user = GetUser({pwd, username});
+        if (user){
+            console.log("hello "+user.name);
+        } else {
+            Alert({msg: "User Name or Password incorrect!", type: "danger"});
         }
     }
 
     return (
         <div className="form-box">
             <h1>Login Here</h1>
+            <div id="alert"></div>
             <form method="post" onSubmit={handleSubmit}>
                 <TextInput icon='fa fa-user' id='username' placeholder='User Name' name='username' errorMessage="Username is required!" required/>
                 <PasswordInput icon='fa fa-key' id='password' placeholder='Password' name='password' errorMessage="Password is required!" required/>
--- src/Server/Users.js
+++ src/Server/Users.js
@@ -2,10 +2,21 @@ import img1 from "./Images/user1.jpg"
 import img2 from "./Images/user2.jpg"
 import img3 from "./Images/user3.jpg"
 
-const users = [
+const Users = [
     {username:"Tigger", nickname:"TY-TY", password:"blabka10@", image: img1},
     {username:"Panda", nickname:"Doobi", password:"Dubigal12!", image: img2},
     {username:"Lion", nickname:"Lio", password:"Roar99$", image: img3}
 ];
 
-export default users;
+
+/*NOT WORKING!*/
+function GetUser({pwd, username}){
+    Users.forEach((user) => {
+        if(user.username.value === username && user.password.value === pwd){
+            return user;
+        }
+    });
+    return null;
+}
+
+export default GetUser;
